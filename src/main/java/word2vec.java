af7f3ae530a09504fef4a29a7bd485adc9345854
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-11-12 12:09:55 +0800

    add Sort function

--- src/main/java/word2vec.java
+++ src/main/java/word2vec.java
@@ -3,7 +3,7 @@
  * word2vec at Song Recommendation (Similar)
  * 
  * version: November 11, 2019 04:40 PM
- * Last revision: November 12, 2019 11:27 AM
+ * Last revision: November 12, 2019 12:09 PM
  * 
  * Author : Chao-Hsuan Ke
  * Institute: Delta Research Center
@@ -40,20 +40,22 @@ public class word2vec
 	BufferedWriter writer;
 	private String output_folder = folder;
 	private String output_file = "lyrics_word2vec.vec";
+	private String title_file = "lyrics_mayday.txt";
+	private ArrayList<String> termTitle = new ArrayList<String>();
 	
-	// Similarity
-	//private ArrayList<double[]> vecValue = new ArrayList();
-	//ArrayList<double[]> vecValue = new ArrayList<double[]>(); 
-	private	ArrayList<java.util.Vector> vecValue = new ArrayList<java.util.Vector>();
 	
 	private String inputStr = "然後 呢 他們 說 你 的 心 似乎 痊癒 了 也 開始 有 個人 為 你 守護 著 我該 心安 或者 心碎 呢 然後 呢 其實 我 的 日子 也還 可以 呢 除了 回憶 肆虐 的 某些 時刻 慶幸 還有 淚水 沖淡 苦澀 而 那些 昨天 仍 繽紛 著 它們 都 有 我 細心 收藏 著 也許 你還 記得 也許 你 都 忘記 了 也 不是 那 麽 重要 了 只 期待 後來 的 你 能 快樂 那 就是 後來 的 我 最想 的 後來 的 我倆 仍 走著 只是 不再 並肩 了 朝 各自 的 人生 追尋 了 無論是 後來 故事 怎麼 麽 了 也 要 讓 後來 人生 精彩 著 後來 的 我倆 我 期待 著 淚水 中能 看見 你 真的 自由 了 親愛 的 回憶 我倆 共同 走過 的 曲折 是 那些 帶 我倆 來到 了 這 一刻 讓 珍貴 的 人生 有失 有 得 用 新 的 幸福 把 遺憾 包著 就 這 麽 朝著 未來 前進 了 有 再 多 的 不 捨 也 要 狠心 割捨 別 回頭 看 我 親愛 的 只 期待 後來 的 你 能 快樂 那 就是 後來 的 我 最想 的 後來 的 我倆 仍 走著 只是 不再 並肩 了 朝 各自 的 人生 追尋 了 無論是 後來 故事 怎麼 麽 了 也 要 讓 後來 人生 精彩 著 後來 的 我倆 我 期待 著 淚水 中能 看見 你 真的 幸福快樂 在 某處 另 一個 你 留下 了 在 那裏 另 一個 我 微笑 著 另 一個 我倆 還 深愛 著 代表 我倆 永遠 著 如果 能 這 麽 想 就夠 了 無論是 後來 故事 怎麼 麽 了 也 要 讓 後來 人生 值得 後來 的 我倆 我 期待 著 淚水 中能 看見 你 真的 自由 了";
 	private double[] inputValue = new double[wordim];
 	private double cosineSimilarity_vale;
-	private java.util.Vector cosineSimilarity_vale_vec;
+	//private java.util.Vector cosineSimilarity_vale_vec;
 	private double[] vector_list_1;	
-	private double[] vector_list_2;
-	
+	private double[] vector_list_2;	
+	// Similarity
 	private java.util.Vector allValue = new java.util.Vector();
+	
+	
+	// Sort
+	private ArrayList<Double> termScore = new ArrayList<Double>();
 	
 	public word2vec() throws Exception
 	{
@@ -68,9 +70,16 @@ public class word2vec
 		
 		// Similarity calculation
 		Str2Vec(inputStr);
-		Read_Vec();
+		Read_VecValue();
+		Read_Title();
 		Comparison(inputValue, allValue);
+		BubbleSort_ArrayList(termScore, termTitle);
 		
+		System.out.println(inputStr);
+		for(int i=0; i<termTitle.size(); i++)
+		{
+			System.out.println(termTitle.get(i)+"	"+termScore.get(i));
+		}
 	}
 	
 	private void Read_text() throws Exception
@@ -142,7 +151,7 @@ public class word2vec
 		}
 	}
 	
-	private void Read_Vec() throws Exception 
+	private void Read_VecValue() throws Exception 
 	{
 		String Line = "";
 		FileReader fr = new FileReader(output_folder + output_file);
@@ -157,25 +166,40 @@ public class word2vec
 		bfr.close();
 	}
 	
-	//private void Comparison(double[] targetVector, List tfidfDocsVector) 
+	private void Read_Title() throws Exception
+	{
+		String Line = "";
+		FileReader fr = new FileReader(output_folder + title_file);
+		bfr = new BufferedReader(fr);
+						
+		String temp[];
+		while((Line = bfr.readLine())!=null)
+		{						
+			//allValue.add(Line);
+			temp = Line.split(" ");
+			termTitle.add(temp[0]);
+		}		
+		
+		fr.close();
+		bfr.close();
+	}
+		
 	private void Comparison(double[] targetVector, java.util.Vector allValue)
 	{	
-		cosineSimilarity_vale_vec = new java.util.Vector();
+		//cosineSimilarity_vale_vec = new java.util.Vector();
 		
 		ArrayList<double[]> tfidfDocsVector = new ArrayList<double[]>();
 		double[] temp;
 		String[] QQ;
 		for(int i=0; i<allValue.size(); i++)
 		{
-			temp = new double[wordim];
-			//System.out.println(allValue.get(i));
+			temp = new double[wordim];			
 			QQ = allValue.get(i).toString().split(",");
 			for(int j=0; j<wordim; j++) {
 				temp[j] = Double.parseDouble(QQ[j]);
 			}
 			tfidfDocsVector.add(temp);
-		}
-		
+		}		
 							
 		vector_list_1 = targetVector;
 
@@ -184,13 +208,14 @@ public class word2vec
 
 			cosineSimilarity_vale = CosineSimilarity(vector_list_1, vector_list_2);
 			if (Double.isNaN(cosineSimilarity_vale)) {
-				cosineSimilarity_vale_vec.add(0);
+				//cosineSimilarity_vale_vec.add(0);
+				termScore.add(0.0);
 			} else {
-				cosineSimilarity_vale_vec.add(cosineSimilarity_vale);
+				//cosineSimilarity_vale_vec.add(cosineSimilarity_vale);
+				termScore.add(cosineSimilarity_vale);
 			}
-			System.out.println(j+" "+cosineSimilarity_vale);
+			//System.out.println(j+" "+cosineSimilarity_vale+"	"+termScore.get(j));
 		}
-
         
 	}
 	
@@ -218,6 +243,31 @@ public class word2vec
         }
         
         return cosineSimilarity;
+	}
+	
+	private void BubbleSort_ArrayList(ArrayList<Double> arrlist, ArrayList<String> arrlistTitle)
+	{		
+		//ArrayList<Double> arr_sort = new ArrayList<Double>();	//arrlist; 
+				
+		double value_temp;
+		String title_temp;
+	
+		for(int j=0; j< arrlist.size(); j++)
+		{			
+			for(int i=j+1; i<arrlist.size(); i++)
+			{
+			    if(arrlist.get(i) > arrlist.get(j)){
+			    	value_temp = arrlist.get(j);
+			    	arrlist.set( j, arrlist.get(i));
+			    	arrlist.set( i, value_temp);
+			    	
+			    	title_temp = arrlistTitle.get(j);
+			    	arrlistTitle.set(j, arrlistTitle.get(i));
+			    	arrlistTitle.set(i, title_temp);
+			   }
+			}
+		}
+		
 	}
 	
 	public static void main( String[] args ) {
